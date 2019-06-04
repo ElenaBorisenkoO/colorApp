eaca31e4255930a833c200ba459a4d71e079d55a
Lena <elena.borisenko1991@gmail.com>
2019-06-04 22:15:48 +0000

    refactor first frame

--- first_frame/first_frame.js
+++ first_frame/first_frame.js
@@ -1,18 +1,5 @@
+const secondFrame = window.parent.$("#secondFrame").contents().find('body');
 
-$("#yellowButton").on({
-    click: function () {
-        window.parent.$("#secondFrame").contents().find('body').css('backgroundColor', 'yellow');
-    }
-})
-
-$("#greenButton").on({
-    click: function () {
-        window.parent.$("#secondFrame").contents().find('body').css('backgroundColor', 'green');
-    }
-})
-
-$("#blueButton").on({
-    click: function () {
-        window.parent.$("#secondFrame").contents().find('body').css('backgroundColor', 'blue');
-    }
-})
+$('button').click(function(event) {
+    secondFrame.css('backgroundColor', event.target.className)
+});
